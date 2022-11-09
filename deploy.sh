09160fd5e7899668876b0803a131b8baf2f0b669
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-10 01:19:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ proj_folder="/var/lib/jenkins/workspace/FinalProj/docker-compose-production.yml"
 
 echo "creating dir and copy"
 scp -o StrictHostKeyChecking=no -r $proj_folder ubuntu@$machine:/home/ubuntu/
+ssh ubuntu@$machine "docker login"
+ssh ubuntu@$machine "docker pull avishilon22/8200dev_final:latest"
 ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml up -d --no-build;sleep 30;docker container ls -a;"
 if [ $machine == "test" ]; then 
     echo 'run Curl test...'
