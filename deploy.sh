c807ab8a8ac354fd9caaf25c170a2e7b5939180c
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-10 00:46:44 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ proj_folder="/var/lib/jenkins/workspace/FinalProj/docker-compose-production.yml"
 
 echo "creating dir and copy"
 scp -o StrictHostKeyChecking=no -r $proj_folder ubuntu@$machine:/home/ubuntu/
-ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml up -d --no-build"
+ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml up -d --no-build;sleep 30;docker container ls -a;"
 if [ $machine == "test" ]; then 
     echo 'run Curl test...'
-    Ans= ssh ubuntu@$machine "curl -i https://127.0.0.1:5000;sleep 20;"
+    Ans= ssh ubuntu@$machine "curl -i https://127.0.0.1:5000"
     if [ Ans > 0 ]; then echo "Request was Successful"
     else echo "failed connection"
     fi
