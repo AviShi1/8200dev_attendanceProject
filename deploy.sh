1c42c97f0b76a322a52b5abe83712089c26bc934
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-09 20:30:45 +0200

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+machine=$1
+
+proj_folder="/var/lib/jenkins/workspace/FinalProj/docker-comepose-production.yml"
+
+echo "creating dir and copy"
+scp -o StrictHostKeyChecking=no -r $proj_folder ubuntu@$machine:/home/ubuntu/
+ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yaml up -d"
+if[ $machine == "test" ];
+then 
+    echo 'run Curl test...'
+    Ans= ssh ubuntu@$machine "Curl test"
+    if [ Ans > 0 ];
+    then echo "Request was Successful"
+    else echo "failed connection"
+    fi
+    ssh ubuntu@$machine "docker-compose-production -f /home/ubuntu/docker-compose-production.yml down"
+    echo 'test docker has stopped!'
+fi
