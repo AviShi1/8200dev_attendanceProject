52ee514afb1858ece734b1efd6b2e7e746b7ef96
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-09 21:26:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ scp -o StrictHostKeyChecking=no -r $proj_folder ubuntu@$machine:/home/ubuntu/
 ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml up -d"
 if [ $machine == "test" ]; then 
     echo 'run Curl test...'
-    Ans= ssh ubuntu@$machine "Curl test"
+    Ans= ssh ubuntu@$machine "curl test"
     if [ Ans > 0 ]; then echo "Request was Successful"
     else echo "failed connection"
     fi
-    ssh ubuntu@$machine "docker-compose-production -f /home/ubuntu/docker-compose-production.yml down"
+    ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml down"
     echo 'test docker has stopped!'
 fi
