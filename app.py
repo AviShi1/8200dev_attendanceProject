adffc7b749b9ed9989fb21af0d09e5df916e7f98
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-10 05:22:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,29 @@ app = Flask(__name__)
 df = csv_connection()
 
 
-@app.route('/')
-def df_table():
-    return render_template('index.html', tables=[df.to_html(classes='data', header="true")])
+#@app.route('/')
+#def df_table():
+#    return render_template('index.html', tables=[df.to_html(classes='data', header="true")])
 
+@app.route('/')
+@app.route('/home')
+def html_table():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/attendance')
+def attendance_table():
+    return render_template('dataframe.html',  tables=[df.to_html(classes='data', header="true")])
+
+
+@app.route('/contact')
+def contact_me():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
