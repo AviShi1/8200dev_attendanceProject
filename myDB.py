b0623f9639d2d22caf567f9afb59e7b23a8ed82b
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-10 06:04:58 +0200

    Update myDB.py

--- myDB.py
+++ myDB.py
@@ -13,7 +13,7 @@ def csv_connection():
     new_df = pd.DataFrame()
 
     db = mysql.connector.connect(
-        host='127.0.0.1',
+        host='db',
         user='root',
         passwd='aviaot2020',
         database='attenDev'
