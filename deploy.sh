0a8e51c826a8260a15ba5fbf61d408580117238f
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-18 00:03:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ ssh ubuntu@$machine "docker pull avishilon22/8200dev_final:latest"
 ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml up -d --no-build;sleep 10;docker container ls -a;"
 if [ $machine == "test" ]; then 
     echo 'run Curl test...'
-    Ans= ssh ubuntu@$machine "curl -Is http://127.0.0.1:5000"
-    if [ Ans > 0 ]; then echo "Request was Successful"
+    Ans= ssh ubuntu@$machine `curl -s -I localhost:5000 | grep HTTP | awk {'print $2'}`
+    if [[ $Ans -eq 200 ]]; then echo "Request was Successful"
     else echo "failed connection"
     fi
     ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml down;docker rmi $(docker images -q);docker system prune -f;"
