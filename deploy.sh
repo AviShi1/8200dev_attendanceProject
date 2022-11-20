5bb80ee36ee2b775a4312a82c5a143afa6d33038
avishilon26 <101514049+avishilon26@users.noreply.github.com>
2022-11-20 19:24:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,9 @@ if [ $machine == "test" ]; then
     echo 'run Curl test...'
     RESPONSE=$(curl -Is http://127.0.0.1:5000 | head -n 1)
     if [ "$RESPONSE"="HTTP/1.1 200 OK" ]; then echo "Request was Successful"
-    else echo "failed connection"
+    else 
+        echo "failed connection"
+        exit 1
     fi
     ssh ubuntu@$machine "docker-compose -f /home/ubuntu/docker-compose-production.yml down;docker rmi $(docker images -q);docker system prune -f;"
     echo 'test docker has stopped!'
